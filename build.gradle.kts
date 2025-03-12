22bd94fca926e478463b208645ccfadaa9caa14f
im.joy <im.joy@dev-lane.com>
2025-03-12 18:10:02 +0900

    kotest 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,21 +2,30 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.9.22"
+    kotlin("plugin.allopen") version "1.9.22"
     application
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+val kotestVersion = "5.8.1"
+
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://mvnrepository.com/artifact/io.kotest") }
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation(kotlin("stdlib"))
+
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-framework-datatest:${kotestVersion}")
+    testImplementation ("io.kotest:kotest-property:$kotestVersion")
 }
 
-tasks.test {
+tasks.withType<Test>().configureEach {
     useJUnitPlatform()
 }
 

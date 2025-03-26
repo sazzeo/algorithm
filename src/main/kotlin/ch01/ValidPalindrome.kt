a5413b9cd3adb0ad5ddd7670a5e48d631acda8d2
im.joy <im.joy@dev-lane.com>
2025-03-26 18:47:40 +0900

    fix: 챕터 1 풀이 추가

--- src/main/kotlin/ch01/ValidPalindrome.kt
+++ src/main/kotlin/ch01/ValidPalindrome.kt
@@ -1,10 +1,48 @@
 package ch01
 
-//https://leetcode.com/problems/valid-palindrome/description/
+// https://leetcode.com/problems/valid-palindrome/description/
 class ValidPalindrome {
-    fun isPalindrome(str: String): Boolean {
-        val uppercase = str.uppercase()
-        val reverse = uppercase.reversed()
-        return uppercase == reverse
+
+    // 39ms
+    fun isPalindrome1(str: String): Boolean {
+        val newStr = str.replace(Regex("[^0-9A-Za-z]"), "")
+            .uppercase()
+        val reverse = newStr.reversed()
+
+        return newStr == reverse
     }
+
+
+    // 32ms
+    fun isPalindrome2(str: String): Boolean {
+        val newStr = str.replace(Regex("[^0-9A-Za-z]"), "")
+            .uppercase()
+        val lastIndex = newStr.length - 1
+        newStr.forEachIndexed { index, it ->
+            if (it != newStr[lastIndex - index]) {
+                return false
+            }
+        }
+        return true
+    }
+
+
+    // 4ms
+    fun isPalindrome3(str: String): Boolean {
+        if (str.isEmpty()) return true
+        var l = 0
+        var r = str.length - 1
+
+        while (l < r) {
+            while (!str[l].isLetterOrDigit() && l < r) l++
+            while (!str[r].isLetterOrDigit() && l < r) r--
+
+            if (str[l].lowercaseChar() != str[r].lowercaseChar()) return false
+            l++
+            r--
+        }
+        return true
+    }
+
+
 }
--- src/test/kotlin/ch01/ValidPalindromeTest.kt
+++ src/test/kotlin/ch01/ValidPalindromeTest.kt
@@ -4,10 +4,10 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 class ValidPalindromeTest : StringSpec({
-
+    val test = ValidPalindrome()
     "문자열 뒤집기 테스트" {
-
-
+        test.isPalindrome3("A man, a plan, a canal: Panama") shouldBe true
+        test.isPalindrome3("race a car") shouldBe false
     }
 
 })

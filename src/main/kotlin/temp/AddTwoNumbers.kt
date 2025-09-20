bbaa3ed444b56c6b780a067dc8ed1ed685564f3b
sazzeo <imjee02@gmail.com>
2025-09-20 23:30:06 +0900

    AddTwoNumbers 추가

---
+++ src/main/kotlin/temp/AddTwoNumbers.kt
@@ -0,0 +1,38 @@
+package temp
+
+import datatype.ListNode
+
+//100자리 숫자는 표현이 안됨
+object AddTwoNumbers {
+    fun addTwoNumbers(l1: ListNode, l2: ListNode): ListNode {
+        var result = ListNode.createList()
+        var nextSum = 0;
+        var l1Node: ListNode? = l1
+        var l2Node: ListNode? = l2
+        var resultNode = result
+
+        while (l1Node != null || l2Node != null || nextSum == 1) {
+            var sum = (l1Node?.`val` ?: 0) + (l2Node?.`val` ?: 0) + nextSum
+
+            if (sum > 9) {
+                nextSum = 1
+                sum = sum - 10
+            } else {
+                nextSum = 0
+            }
+
+            resultNode.`val` = sum
+
+            l1Node = l1Node?.next
+            l2Node = l2Node?.next
+            if(l1Node == null && l2Node == null && nextSum != 1) {
+                break
+            }
+
+            resultNode.next = ListNode.createList()
+            resultNode = resultNode.next!!
+        }
+
+        return result
+    }
+}
---
+++ src/test/kotlin/temp/AddTwoNumbersTest.kt
@@ -0,0 +1,59 @@
+package temp
+
+import datatype.ListNode
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class AddTwoNumbersTest : StringSpec({
+
+//    Input: l1 = [2,4,3,4], l2 = [5,6,4,4]
+//    Output: [7,0,8]
+//    Explanation: 342 + 465 = 807
+    "solution1" {
+        val l1 = ListNode.createList(2, 4, 3)
+        val l2 = ListNode.createList(5, 6, 4)
+        val result = AddTwoNumbers.addTwoNumbers(l1, l2)
+
+        result shouldBe ListNode.createList(7, 0, 8)
+    }
+
+//    Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+//    Output: [8,9,9,9,0,0,0,1]
+    "solution2" {
+        val l1 = ListNode.createList(9,9,9,9,9,9,9)
+        val l2 = ListNode.createList(9,9,9,9)
+        val result = AddTwoNumbers.addTwoNumbers(l1, l2)
+
+        result shouldBe ListNode.createList(8,9,9,9,0,0,0,1)
+    }
+
+//1,2,3,4,5
+    "test1" {
+        val list = ListNode.createList()
+        list.`val` = 1;
+        list.next = ListNode.createList(2)
+        list.next?.next = ListNode.createList(3)
+        list.next?.next?.next = ListNode.createList(4)
+        list.next?.next?.next?.next = ListNode.createList(5)
+        list shouldBe ListNode.createList(1, 2, 3, 4, 5)
+    }
+
+    "test2" {
+        val list = listOf(1,2,3,4,5)
+        var next: ListNode? = null
+        val result = ListNode.createList()
+        for (l in list) {
+//            next.`val` = l;
+//            next = result.next
+
+
+        }
+
+        result.`val` = 1;
+        result.next = ListNode.createList(2)
+        result.next?.next = ListNode.createList(3)
+        result.next?.next?.next = ListNode.createList(4)
+        result.next?.next?.next?.next = ListNode.createList(5)
+        result shouldBe ListNode.createList(1, 2, 3, 4, 5)
+    }
+})
